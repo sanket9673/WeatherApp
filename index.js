eace748979943ccea66cc077e0381430c307e221
sanket9673 <sanketch9673@gmail.com>
2024-02-21 20:49:00 +0530

    Error Updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function checkWeather(city){
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = math.round(data.main.temp) + "°C";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
